ce4cd4b80528cff4bfd8a947e5eda813f824cb9a
nivekt24 <kvvt24@gmail.com>
2023-11-04 15:33:30 -0700

    sun & moon/style added

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
+  document.body.classList.toggle('switch');
 });
