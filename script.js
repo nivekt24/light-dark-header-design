5f3e6187c5a68b874fd1d7765fadf2695d948ca7
nivekt24 <kvvt24@gmail.com>
2023-11-05 14:18:01 -0800

    added desktop menu/styles

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
-  document.body.classList.toggle('switch');
 });
