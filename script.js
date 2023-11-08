3850795966408a1cb76f044759755de4d46632be
nivekt24 <kvvt24@gmail.com>
2023-11-07 18:28:07 -0800

    light-dark-mode-JS-animation / styles added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const sunMoonContainer = document.querySelector('.sun-moon-container');
 const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
+  const currentRotation = parseInt(
+    getComputedStyle(sunMoonContainer).getPropertyValue('--rotation')
+  );
+  sunMoonContainer.style.setProperty('--rotation', currentRotation + 180);
 });
